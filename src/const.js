916fa1a97445ba76b26859d6bd1d4d434c259859
Shuric <shuric110@yandex.ru>
2020-08-18 19:46:58 +0300

    Sort view - формирование по описанию, обработка клика (WIP)

--- src/const.js
+++ src/const.js
@@ -1,1 +1,22 @@
 export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+export const SORT_TYPES = {
+  'default': {
+    title: `DEFAULT`,
+    compare: null
+  },
+  'date-up': {
+    title: `DATE up`,
+    compare(a, b) {
+      return (!a ? 0 : a.getTime())
+        - (!b ? 0 : b.getTime());
+    }
+  },
+  'date-down': {
+    title: `DATE down`,
+    compare(a, b) {
+      return (!b ? 0 : b.getTime())
+        - (!a ? 0 : a.getTime());
+    }
+  },
+};
--- src/view/sort.js
+++ src/view/sort.js
@@ -1,13 +1,29 @@
 import ComponentView from "./component.js";
+import {SORT_TYPES} from "../const.js";
 
 export default class Sort extends ComponentView {
+  constructor() {
+    super();
+    this._sortChangeHandler = this._sortChangeHandler.bind(this);
+  }
+
   getTemplate() {
     return `
       <div class="board__filter-list">
-        <a href="#" class="board__filter">SORT BY DEFAULT</a>
-        <a href="#" class="board__filter">SORT BY DATE up</a>
-        <a href="#" class="board__filter">SORT BY DATE down</a>
+        ${Object.entries(SORT_TYPES).map(([code, {title}]) => `
+          <a href="#" class="board__filter" data-sort="${code}">SORT BY ${title}</a>
+        `).join(``)}
       </div>
     `;
+  }
+
+  _sortChangeHandler(evt) {
+    evt.preventDefault();
+    this._callback.sortChange();
+  }
+
+  setSortChangeHandler(callback) {
+    this.getElement().addEventListener(`click`, this._sortChangeHandler);
+    this._callback.sortChange = callback;
   }
 }
